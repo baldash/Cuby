c09109ee21cef83203571960b7717ec3f4e4de12
baldash <hugo.baldassin@epitech.eu>
2018-10-09 03:14:53 +0200

    [PROJECT INIT]

---
+++ app/src/main/java/com/keimyung/baldash/cuby/MainActivity.java
@@ -0,0 +1,13 @@
+package com.keimyung.baldash.cuby;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

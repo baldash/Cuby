9f860825bc0dcafbea3f15f2e90a5d6655609bbb
baldash <hugo.baldassin@epitech.eu>
2018-10-11 06:47:10 +0200

    [ADD] sprite class to handle sprite drawing easily;

---
+++ app/src/main/java/com/keimyung/baldash/cuby/Sprite.java
@@ -0,0 +1,61 @@
+package com.keimyung.baldash.cuby;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+public class Sprite {
+
+    private Bitmap bmp;
+    private Point pos;
+    private Rect dstRect;
+
+    public Sprite(String fileName, Point pos)
+    {
+        bmp = ResourcesHandler.getInstance().getResourceByName(fileName);
+        this.pos = pos;
+        dstRect = new Rect(pos.x, pos.y, bmp.getWidth() + pos.x, bmp.getHeight() + pos.y);
+    }
+
+    ///// GETTERS
+
+    public Point getPos()
+    {
+        return pos;
+    }
+
+    public int getWidth()
+    {
+        return bmp.getWidth();
+    }
+
+    public int getHeight()
+    {
+        return bmp.getHeight();
+    }
+
+    public Bitmap getBmp()
+    {
+        return bmp;
+    }
+
+    public Rect getSpriteRect()
+    {
+        return dstRect;
+    }
+
+    ///// SETTERS
+
+    public void setPos(Point newPos)
+    {
+        pos = newPos;
+        dstRect.set(pos.x, pos.y, bmp.getWidth() + pos.x, bmp.getHeight() + pos.y);
+    }
+
+    public void setBmp(String fileName)
+    {
+        bmp = ResourcesHandler.getInstance().getResourceByName(fileName);
+        dstRect.set(pos.x, pos.y, bmp.getWidth() + pos.x, bmp.getHeight() + pos.y);
+    }
+
+}
